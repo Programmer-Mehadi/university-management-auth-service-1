d3125ffc85431dc1eff1eb53a91b2c906c88fc9e
Programmer-Mehadi <mehadi.developer@gmail.com>
2023-06-05 13:02:07 +0600

    fff

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-
+import usersRouter from './modules/users/users.route'
 const app: Application = express()
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 // comment
+
+app.use('/api/v1/users/', usersRouter)
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
